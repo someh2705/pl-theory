787cee26e54991658ddaa9891e2f10cb897090b2
someh2705 <someh2705@gmail.com>
2024-10-06 13:35:02 +0000

    create projects

---
+++ project/c/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+    const char* message = "Hello World!(C)";
+    printf("%s\n", message);
+    
+    return 0;
+}
---
+++ project/cpp/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(void)
+{
+    std::string message { "Hello World!(C++)" };
+    std::cout << message << std::endl;
+    
+    return 0;
+}
